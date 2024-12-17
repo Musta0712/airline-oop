1659eea2551e783f2c67ed968ad0c877b8e6f077
Adri <adri@B25>
2024-12-17 11:13:46 +0100

    model and components fact (App no)

---
+++ src/org/ies/airline/components/AirlineApp.java
@@ -0,0 +1,15 @@
+package org.ies.airline.components;
+
+import java.util.Scanner;
+
+public class AirlineApp {
+    private final Scanner scanner;
+    private final AirlineReader airlineReader;
+
+    public AirlineApp(Scanner scanner, AirlineReader airlineReader) {
+        this.scanner = scanner;
+        this.airlineReader = airlineReader;
+    }
+
+
+}
